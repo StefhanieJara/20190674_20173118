b3f4048a108c85adaa23e36116310e28c8a1d2c8
joelsilvera <joel.silvera@pucp.edu.pe>
2022-06-06 20:37:40 -0500

    query good

--- src/main/java/com/example/_20190674_20173118/Daos/CelebridadDaos.java
+++ src/main/java/com/example/_20190674_20173118/Daos/CelebridadDaos.java
@@ -24,7 +24,7 @@ public class CelebridadDaos {
 
         try (Connection connection = DriverManager.getConnection(url, user, pass);
              Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery("select * from (select a.actor_id, concat(first_name,\" \",last_name) as \"Nombre\",count(a.actor_id) as \"N° Peliculas\" from actor a inner join film_actor f on (a.actor_id= f.actor_id) group by f.actor_id) as t inner join  (select a.actor_id, count(a.actor_id) as \"N° Categorias\" from actor a inner join film_actor f on (a.actor_id= f.actor_id) inner join film fi on (f.film_id= fi.film_id) inner join film_category ca on (fi.film_id= ca.film_id) inner join category cat on (ca.category_id=cat.category_id) group by a.actor_id) as t2 on (t2.actor_id= t.actor_id)  ");) {
+             ResultSet rs = stmt.executeQuery("select * from (select a.actor_id, concat(first_name,'',last_name) as 'Nombre',count(a.actor_id) as 'N° Peliculas' from actor a inner join film_actor f on (a.actor_id= f.actor_id) group by f.actor_id) as t inner join  (select a.actor_id, count(a.actor_id) as 'N° Categorias' from actor a inner join film_actor f on (a.actor_id= f.actor_id) inner join film fi on (f.film_id= fi.film_id) inner join film_category ca on (fi.film_id= ca.film_id) inner join category cat on (ca.category_id=cat.category_id) group by a.actor_id) as t2 on (t2.actor_id= t.actor_id) ")){
 
             while (rs.next()) {
                 celebridadBean bPersona = new celebridadBean();
