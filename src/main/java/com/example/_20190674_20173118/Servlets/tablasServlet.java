90b097fd38a2ff2da836d2bc137f537fb6a20676
joelsilvera <joel.silvera@pucp.edu.pe>
2022-06-06 19:44:09 -0500

    Nuevo commit servlet

---
+++ src/main/java/com/example/_20190674_20173118/Servlets/tablasServlet.java
@@ -0,0 +1,19 @@
+package com.example._20190674_20173118.Servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "tablasServlet", value = "/tablasServlet")
+public class tablasServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
