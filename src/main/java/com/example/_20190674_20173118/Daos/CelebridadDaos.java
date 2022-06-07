9ccff3ce3aed3ba6c4b4a33563dbeedcaab748d4
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2022-06-06 19:42:19 -0500

    carpeta

--- src/main/java/com/example/_20190674_20173118/Daos/CelebridadDaos.java
+++ src/main/java/com/example/_20190674_20173118/Daos/CelebridadDaos.java
@@ -2,6 +2,12 @@ package com.example._20190674_20173118.Daos;
 
 public class CelebridadDaos {
 
+    private int actor_id;
+    private String first_name;
+    private String last_name;
+
+
+
 
 
 }
